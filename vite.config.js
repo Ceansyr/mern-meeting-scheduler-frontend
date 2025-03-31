9509b0cc4e3c405c292debb8d8a881016fe58c17
Ceansyr <chinmaymandavkar@gmail.com>
2025-03-31 12:02:32 +0530

    vite.config.js issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true,  // Ensures React Router handles routing
+    port: 5173,
+  },
+  build: {
+    outDir: "dist",
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
   },
 });
